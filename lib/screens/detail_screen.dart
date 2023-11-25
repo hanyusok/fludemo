c4835c780e9433324847455325bdbbfb6e53ae4a
hanyusok <hanyusok@gmail.com>
2023-11-26 00:37:38 +0900

    Update detail_screen.dart

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,50 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:flm/models/book.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+  final Book book;
+  const DetailScreen({super.key, required this.book});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('한 권이면 충분해, 프로툴스'),
+        title: Text(book.title),
       ),
       body: Column(
         children: [
-          Image.network(
-              'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FnBSOU%2FbtrNb9iQ2X4%2FBtNV0ZUfJeLaUBUKckf3gK%2Fimg.jpg'),
+          Image.network(book.image),
           const Padding(padding: EdgeInsets.all(3)),
           Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
                   padding: const EdgeInsets.all(10),
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Container(
-                          child: Text(
-                            '한 권이면 충분해, 프로툴스',
-                            style: TextStyle(
-                                fontSize: 21, fontWeight: FontWeight.bold),
-                          ),
+                        Text(
+                          book.title,
+                          style: const TextStyle(
+                              fontSize: 21, fontWeight: FontWeight.bold),
                         ),
-                        const Text('부제목1 부제목1 부제목1',
-                            style: TextStyle(
-                              fontSize: 18,
+                        Text(book.subtitle,
+                            style: const TextStyle(
+                              fontSize: 15,
                               color: Colors.grey,
                             )),
-                        Container(
-                            padding: const EdgeInsets.all(10),
-                            child: Center(
-                                child: Icon(
-                              Icons.star,
-                              color: Colors.red,
-                            )))
-                      ]))
+                      ])),
+              Container(
+                  padding: const EdgeInsets.all(10),
+                  child: const Center(
+                      child: Icon(
+                    Icons.star,
+                    color: Colors.red,
+                  )))
             ],
-          )
+          ),
+          const Padding(padding: EdgeInsets.all(3)),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Icon(
+                    Icons.call,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    '전화',
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.near_me,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    '라우트',
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.save,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    '저장',
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              )
+            ],
+          ),
+          Container(
+              padding: const EdgeInsets.all(15), child: Text(book.description))
         ],
       ),
     );
