4ea449d64d44c3f4803379bac87120f83c356232
hanyusok <hanyusok@gmail.com>
2023-11-27 11:09:29 +0900

    Update detail_screen.dart

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flm/models/book.dart';
+import 'package:fludemo/models/book.dart';
 
 class DetailScreen extends StatelessWidget {
   final Book book;
@@ -11,85 +11,88 @@ class DetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(book.title),
       ),
-      body: Column(
-        children: [
-          Image.network(book.image),
-          const Padding(padding: EdgeInsets.all(3)),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          book.title,
-                          style: const TextStyle(
-                              fontSize: 21, fontWeight: FontWeight.bold),
-                        ),
-                        Text(book.subtitle,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(book.image),
+            const Padding(padding: EdgeInsets.all(3)),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            book.title,
                             style: const TextStyle(
-                              fontSize: 15,
-                              color: Colors.grey,
-                            )),
-                      ])),
-              Container(
-                  padding: const EdgeInsets.all(10),
-                  child: const Center(
-                      child: Icon(
-                    Icons.star,
-                    color: Colors.red,
-                  )))
-            ],
-          ),
-          const Padding(padding: EdgeInsets.all(3)),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  Icon(
-                    Icons.call,
-                    color: Colors.blue,
-                  ),
-                  Text(
-                    '전화',
-                    style: TextStyle(color: Colors.blue),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(
-                    Icons.near_me,
-                    color: Colors.blue,
-                  ),
-                  Text(
-                    '라우트',
-                    style: TextStyle(color: Colors.blue),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(
-                    Icons.save,
-                    color: Colors.blue,
-                  ),
-                  Text(
-                    '저장',
-                    style: TextStyle(color: Colors.blue),
-                  )
-                ],
-              )
-            ],
-          ),
-          Container(
-              padding: const EdgeInsets.all(15), child: Text(book.description))
-        ],
+                                fontSize: 21, fontWeight: FontWeight.bold),
+                          ),
+                          Text(book.subtitle,
+                              style: const TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey,
+                              )),
+                        ])),
+                Container(
+                    padding: const EdgeInsets.all(10),
+                    child: const Center(
+                        child: Icon(
+                      Icons.star,
+                      color: Colors.red,
+                    )))
+              ],
+            ),
+            const Padding(padding: EdgeInsets.all(3)),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Icon(
+                      Icons.call,
+                      color: Colors.blue,
+                    ),
+                    Text(
+                      '전화',
+                      style: TextStyle(color: Colors.blue),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.near_me,
+                      color: Colors.blue,
+                    ),
+                    Text(
+                      '라우트',
+                      style: TextStyle(color: Colors.blue),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(
+                      Icons.save,
+                      color: Colors.blue,
+                    ),
+                    Text(
+                      '저장',
+                      style: TextStyle(color: Colors.blue),
+                    )
+                  ],
+                )
+              ],
+            ),
+            Container(
+                padding: const EdgeInsets.all(15),
+                child: Text(book.description))
+          ],
+        ),
       ),
     );
   }
